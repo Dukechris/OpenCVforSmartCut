b5f6cd5a1556450cd83ab77a785f1471e879858f
Zhangxiao <zhangxiao@zhangxiaodemacbook-pro.local>
2015-10-09 08:38:39 +0800

    Initial Commit

---
+++ OpenCVTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCVTest
+//
+//  Created by Zhangxiao on 15/10/9.
+//  Copyright © 2015年 Zhangxiao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
